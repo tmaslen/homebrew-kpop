f6c6380d466806449b0eeb14d39d58a14c41eb78
tmaslen <tom_maslen@hotmail.com>
2018-06-14 17:35:07 +0100

    Trying to integrate with brew tap

---
+++ kpop.rb
@@ -0,0 +1,15 @@
+class Kpop < Formula
+  desc "Mac CLI utility to quickly kill a process that is hogging a port"
+  version 0.2
+  homepage ""
+  url "https://github.com/tmaslen/kpop/archive/0.2.zip"
+  sha256 "1ed1f832baa759f9dfb53dc362fd6b1f2ee21a7e88cd8c75a1d77a204be5cc92"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
